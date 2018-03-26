f658b8bbeff6f9d0f2f98ff32781d84e6f65551c
nelsonh88 <nelson.huezo88@gmail.com>
2018-03-26 10:56:42 -0400

    Adds curl scripts and AJAX
    - Worked on users curl scripts
    - Made folders for resources' curl and client side scripts
    
    Future:  Test scripts waiting on API merge

---
+++ assets/scripts/users/api.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const config = require('./config.js')
+const store = require('./store')
+
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-up/',
+    method: 'POST',
+    headers: {
+      contentType: 'application/json'
+    },
+    data: data
+  })
+}
+
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-in/',
+    method: 'POST',
+    headers: {
+      contentType: 'application/json'
+    },
+    data: data
+  })
+}
+
+const changePassword = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/change-password/' + store.user.id,
+    method: 'PATCH',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: data
+  })
+}
+
+const signOut = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-out/' + store.user.id,
+    method: 'DELETE',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+}
