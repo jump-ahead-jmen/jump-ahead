f96b71ad5553715af1b852cd2aa96606f3e60b23
nelsonh88 <nelson.huezo88@gmail.com>
2018-03-26 12:42:37 -0400

    Adds the auth for users
    - created a events action for onSignIn, onChangePassword, onSignOut
    - created the ui actions for success and failures
    
    Co-authored-by: Jonny Hicks <jonathankhicks@gmail.com>

---
+++ assets/scripts/users/ui.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const store = require('../store')
+// above is for the token as well
+
+const signUpSuccess = function (data) {
+  $('#message').text('Signed up Successfully!')
+  $('#message').css('background-color', 'green')
+  console.log(data)
+}
+
+const signUpFailure = function (error) {
+  console.log(error)
+  $('#message').text('Error on signing up!')
+  $('#message').css('background-color', 'red')
+}
+const signInSuccess = function (data) {
+  $('#message').text('Signed in Successfully!')
+  $('#message').css('background-color', 'green')
+  console.log(data)
+  // below is for the token
+  store.user = data.user
+}
+
+const signInFailure = function (error) {
+  console.log(error)
+  $('#message').text('Error on signing in!')
+  $('#message').css('background-color', 'red')
+}
+
+const changePasswordSuccess = function (data) {
+  $('#message').text('Change password Successfully!')
+  $('#message').css('background-color', 'green')
+}
+
+const changePasswordFailure = function (error) {
+  console.log(error)
+  $('#message').text('Error on changing password!')
+  $('#message').css('background-color', 'red')
+}
+
+const signOutSuccess = function (data) {
+  $('#message').text('Signed out Successfully!')
+  $('#message').css('background-color', 'green')
+}
+
+const signOutFailure = function (error) {
+  console.log(error)
+  $('#message').text('Error on signing out!')
+  $('#message').css('background-color', 'red')
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure
+}
