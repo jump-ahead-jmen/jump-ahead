b0f49fe78062ce024f299c103bb4eb327b9b78d1
sonmaximum <maxjuchheim@gmail.com>
2018-03-28 11:29:08 -0400

    Adds function to delete webpage
    Delete webpage now deletes the page (but we need to that to the user)
    
    Co-authored-by: Nelson Huezo <nelson.huezo88@gmail.com>

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,13 +5,14 @@ const userEvents = require('./users/events.js')
 const webpageEvents = require('./webpages/events.js')
 const blogPostEvents = require('./blogposts/events.js')
 const userDropdown = require('./users/user-dropdown.js')
-const getFormFields = require('../../lib/get-form-fields')
+const deleteEvents = require('./delete-events.js')
 
 $(() => {
   userDropdown()
   userEvents.addHandlers()
   webpageEvents.addHandlers()
   blogPostEvents.addHandlers()
+  deleteEvents.addHandlers()
 })
 
 // use require with a reference to bundle the file and use it in this file
